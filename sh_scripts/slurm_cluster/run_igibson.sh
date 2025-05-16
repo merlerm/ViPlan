36db5cbb0ffe62f5f516e263fc9781cd89bbb903
nicoladainese96 <43784881+nicoladainese96@users.noreply.github.com>
2025-05-16 14:13:34 +0300

    Update run_igibson.sh

--- sh_scripts/slurm_cluster/run_igibson.sh
+++ sh_scripts/slurm_cluster/run_igibson.sh
@@ -28,7 +28,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-SCRIPT_DIR=$PWD/"sh_scripts/slurm_cluser/scripts"
+SCRIPT_DIR=$PWD/"sh_scripts/slurm_cluster/scripts"
 echo "Script directory: $SCRIPT_DIR"
 
 if [ ${#forward_args[@]} -gt 0 ]; then
